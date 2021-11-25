102ff7b82c0da2dd6492c05378ded0bfa6b03ad0
Ibuki-Matsumoto <n4rvs3apex@gmail.com>
2021-11-25 17:15:21 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with aws
+          this is ver.2 !
         </a>
       </header>
     </div>
